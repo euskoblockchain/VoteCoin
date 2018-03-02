e6aa8f4a3471af6347e4d6c1b9da7e15c0f45abd
Tomas M <tomas@slax.org>
2018-03-02 13:44:48 +0100

    branding

--- src/init.cpp
+++ src/init.cpp
@@ -828,7 +828,7 @@ bool AppInit2(boost::thread_group& threadGroup, CScheduler& scheduler)
     fLogIPs = GetBoolArg("-logips", false);
 
     LogPrintf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-    LogPrintf("Zcash version %s (%s)\n", FormatFullVersion(), CLIENT_DATE);
+    LogPrintf("VoteCoin version %s (%s)\n", FormatFullVersion(), CLIENT_DATE);
 
     // when specifying an explicit binding address, you want to listen on it
     // even when -connect or -proxy is specified
