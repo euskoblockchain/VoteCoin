d2d4cba3081929409d911a1a4c4679d47d70b981
Tomas M <tomas@slax.org>
2018-10-15 09:58:52 +0200

    no longer need for win32 build patches

--- zcutil/votecoin_build_win32.sh
+++ zcutil/votecoin_build_win32.sh
@@ -6,8 +6,6 @@ cd "$(dirname "$(readlink -f "$0")")"/..    #'"%#@!
 #sudo update-alternatives --config x86_64-w64-mingw32-gcc # (configure to use POSIX variant)
 #sudo update-alternatives --config x86_64-w64-mingw32-g++ # (configure to use POSIX variant)
 
-patch -p1 < ./zcutil/extra/zcash-win32.patch
-
 HOST=x86_64-w64-mingw32 ./zcutil/build.sh --disable-tests || exit 1
 
 cd src
