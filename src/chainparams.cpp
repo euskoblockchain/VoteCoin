172fb525d0bbb6987f7d16a8f0337a1141afa57b
euskoblockchain <61253093+euskoblockchain@users.noreply.github.com>
2020-06-13 11:52:59 +0200

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -120,7 +120,7 @@ public:
         consensus.vUpgrades[Consensus::UPGRADE_BLOSSOM].nActivationHeight = 477777;
 
         // The best chain should have at least this much work.
-        consensus.nMinimumChainWork = uint256S("0x00000000000000000000000000000000000000000000000000002a3fc23b2bbb");
+        consensus.nMinimumChainWork = uint256S("0000000000000000000000000000000000000000000000000000000100010001");
 
         /**
          * The message start string should be awesome! ⓩ❤
